18d208dc5874dd3645867aa6f31f63183532c0c7
grav0211 <grav0211@algonquinlive.com>
2017-01-08 17:32:49 -0500

    Updated PHP and Added 2nd Facebook Widget

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -1,9 +1,12 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
+if(empty($_POST['name'])            ||
+   empty($_POST['phone'])           ||
+   empty($_POST['email'])           ||
+   empty($_POST['groupsize'])       ||
+   empty($_POST['preferreddate'])   ||
+   empty($_POST['location'])        ||
+   empty($_POST['message'])         ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
@@ -11,14 +14,17 @@ if(empty($_POST['name'])      ||
    }
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$group_size = strip_tags(htmlspecialchars($_POST['groupsize']));
+$preferred_date = strip_tags(htmlspecialchars($_POST['preferreddate']));
+$location = strip_tags(htmlspecialchars($_POST['location']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'agrav065@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info.mobileescapes@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nGroup Size: $group_size\n\nPreferred Date: $preferred_date\n\nLocation: $location\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
