e682960823f7b976ad94df7d52c3511968af055a
grav0211 <grav0211@algonquinlive.com>
2017-01-22 16:27:55 -0500

    Form Testing

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -22,7 +22,7 @@ $location = strip_tags(htmlspecialchars($_POST['location']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'info.mobileescapes@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'agrav065@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nGroup Size: $group_size\n\nPreferred Date: $preferred_date\n\nLocation: $location\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
